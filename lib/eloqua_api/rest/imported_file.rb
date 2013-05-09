a710215b3b1a44f65431a2833e93edd77003aa16
icebreaker <theicebreaker007@gmail.com>
2013-05-09 15:57:48 +0300

    added folder support

--- lib/eloqua_api/rest/imported_file.rb
+++ lib/eloqua_api/rest/imported_file.rb
@@ -19,5 +19,25 @@ module Eloqua
     def delete_imported_file(id)
       delete("assets/importedFile/%s" % id)
     end
+
+    def imported_file_folder(id, options={})
+      options[:depth] ||= 'minimal'
+
+      get("assets/importedFile/folder/%s" % id, options)
+    end
+
+    def imported_file_folder_folders(id, options={})
+      options[:count] ||= 10
+      options[:depth] ||= 'minimal'
+
+      get("assets/importedFile/folder/%s/folders" % id, options)
+    end
+
+    def imported_file_folders(options={})
+      options[:count] ||= 10
+      options[:depth] ||= 'minimal'
+
+      get("assets/importedFile/folders", options)
+    end
   end
 end
